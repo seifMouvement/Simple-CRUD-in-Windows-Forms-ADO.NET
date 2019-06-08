53b092434b747a05cd7f138667f98c18cb6a9132
Seif Ben Amor <seifbenamor88@gmail.com>
2019-06-08 00:57:10 -0400

    gerer les doublons lors de l ajout

--- Simple CRUD in Windows Form ADO/Form1.cs
+++ Simple CRUD in Windows Form ADO/Form1.cs
@@ -61,11 +61,25 @@ namespace Simple_CRUD_in_Windows_Form_ADO
             /* insérer dans la BD*/
             d.connecter();
             d.cmd.Connection = d.con;
-            d.cmd.CommandText="INSERT INTO CATEGORIE (NUMCAT,NOMCAT) VALUES ("+int.Parse(txtNUMCAT.Text)+
-                ",'"+txtNOMCAT.Text+"')";
-            d.cmd.ExecuteNonQuery();
-            d.deconnecter();
-            MessageBox.Show("Ajout éffectué avec succes");
+            /*on ba gérer les doublons, il va lancer une requet SELECT qui compte le nombre de ligne
+             ayant le numéro de categorie est egale à la valeur saisie dans le champ numéro.
+             si le nombre obtenur par la requet SQL est superieur à 0 il ne vas pas faire l'insertion
+             car il existe deja un enregistrement avec dans la BD avec ce numéro de categorie*/
+            d.cmd.CommandText = "SELECT COUNT (*) FROM CATEGORIE WHERE NUMCAT="+ int.Parse(txtNUMCAT.Text);
+            int cpt= int.Parse(d.cmd.ExecuteScalar().ToString());
+            if(cpt>0)
+            {
+                MessageBox.Show("Le numéro de catégorie"+txtNUMCAT.Text+"existe déjà");
+            }
+            else
+            { 
+                d.cmd.CommandText="INSERT INTO CATEGORIE (NUMCAT,NOMCAT) VALUES ("+int.Parse(txtNUMCAT.Text)+
+                    ",'"+txtNOMCAT.Text+"')";
+                d.cmd.ExecuteNonQuery();
+                d.deconnecter();
+                MessageBox.Show("Ajout éffectué avec succes");
+
+            }
         }
     }
 }
